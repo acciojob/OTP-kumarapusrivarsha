08a43e879ccedf619d279f440267e5195e582d78
kumarapusrivarsha <134908628+kumarapusrivarsha@users.noreply.github.com>
2026-07-19 13:04:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,80 @@
 //your JS code here. If required.
+const codes = document.querySelectorAll(".code");
+
+
+// Focus first input automatically
+
+codes[0].focus();
+
+
+
+codes.forEach((code, index) => {
+
+
+    code.addEventListener("input", (e) => {
+
+
+        // Allow only numbers
+
+        code.value = code.value.replace(/[^0-9]/g, "");
+
+
+
+        if(code.value !== ""){
+
+
+            // Move to next input
+
+            if(index < codes.length - 1){
+
+                codes[index + 1].focus();
+
+            }
+
+        }
+
+
+    });
+
+
+
+
+
+    code.addEventListener("keydown", (e) => {
+
+
+        if(e.key === "Backspace"){
+
+
+            if(code.value === ""){
+
+
+                // Move to previous input
+
+                if(index > 0){
+
+                    codes[index - 1].focus();
+
+                }
+
+
+            }
+
+            else{
+
+
+                // Clear current input
+
+                code.value = "";
+
+            }
+
+
+        }
+
+
+    });
+
+
+
+});
